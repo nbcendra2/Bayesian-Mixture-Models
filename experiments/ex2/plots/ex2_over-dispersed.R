291f49edf28d1ca074e398a48bc900220b8af15f
nbcendra2 <nigelcendra8@gmail.com>
2025-03-03 22:20:25 +0000

    change plots

--- experiments/ex2/plots/ex2_over-dispersed.R
+++ experiments/ex2/plots/ex2_over-dispersed.R
@@ -1,4 +1,5 @@
 source("code/Bayes_mix_poisson.R")
+source("code/Bayes_mix_normal.R")
 require(coda)
 
 
@@ -22,9 +23,9 @@ var(y_van) #14.35569
 # Poisson Mixture Models
 # -------------------------------------------------
 # Change to desired K components
-K = 20
+K = 50
 # Change to desired alpha parameter 
-alpha_p = 1
+alpha_p = 0.1
 grid <- seq(min(obs_data) - 1, max(obs_data) + 1, by = 1)
 res_pois <- pmm(y = obs_data, grid = grid, K = K, alpha = 1, beta = 1, alpha_dir = rep(alpha_p,K))
 density_pmm <- res_pois$density 
@@ -56,17 +57,16 @@ density_df <- data.frame(
 )
 
 # plot
+offset <- 0.1
+obs_density <- table(factor(obs_data, levels = grid)) / length(obs_data) 
 y_max <- max(c(mean_density, obs_density)) * 1
 plot(grid, mean_density, type = "h", lwd = 2, col = "gray",
-     xlab = "y", ylab = "Density", ylim = c(0, y_max),
-     main = paste("Poisson Mixture Model Density for K =", K))
+     xlab = "y", ylab = "Density", ylim = c(0, y_max))
 
 # Add shaded area for the 95% credible interval
 polygon(c(grid, rev(grid)),
         c(lower_density, rev(upper_density)),
         col = rgb(30/255, 144/255, 255/255, 0.3), border = NA)
-offset <- 0.1
-obs_density <- table(factor(obs_data, levels = grid)) / length(obs_data) 
 points(grid + offset, obs_density, type = "h", lwd = 2, col = "black", lty = 3)  
 legend("topright", legend = c("Mean Density", "95% CI", "Observed Data"),
        col = c("gray", rgb(0, 0, 1, 0.2), "black"), lty = c(1, NA, 3),
@@ -76,7 +76,7 @@ legend("topright", legend = c("Mean Density", "95% CI", "Observed Data"),
 # Square root Gaussian Mixture Models
 # -------------------------------------------------
 grid_norm <- seq(min(obs_data) - 1, max(obs_data) + 1, len = 2000)
-K = 20
+K = 50
 fit_norm <- gmm(y = sqrt(obs_data), grid = grid_norm, K = K, amu = 0, b2mu = 100, asigma2 = 0.1,
             bsigma2 = 1, alpha = rep(alpha_p, K), nsim = 5000, nburn = 1000)
 density_sqrt_norm <- fit_norm$density
@@ -101,6 +101,7 @@ dens3h <- apply(fit_norm$density, 2, quantile, prob = 0.975)
 dfhist <- data.frame(y = sqrt(obs_data))
 dfdens3 <- data.frame(dm = dens3m, dl = dens3l, dh = dens3h,
                       seqgrid = grid_norm)
+require(ggplot2)
 ggplot(dfdens3, aes(x = seqgrid, y = dm)) + geom_line(size = 1, colour = "blue") +
   geom_ribbon(data = dfdens3, aes(x = seqgrid, ymin = dl, ymax = dh),
               alpha = 0.3, fill = "dodgerblue1") + xlab("y") + ylab("Density") + geom_histogram(data = dfhist, aes(x = y,
